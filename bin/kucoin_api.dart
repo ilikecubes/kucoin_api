057f733bfe97342529c1a5c5e2ee1328d46af27a
Kevin <scheeren@proton.me>
2022-12-06 16:17:21 +0100

    Initial Commit

---
+++ bin/kucoin_api.dart
@@ -0,0 +1,5 @@
+import 'package:kucoin_api/kucoin_api.dart' as kucoin_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kucoin_api.calculate()}!');
+}
---
+++ test/kucoin_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:kucoin_api/kucoin_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
